005f8ec1eb2e360ac8c2a436b2ef1c1ac2445e44
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-23 18:50:34 +0000

    Added test for getcode

--- sites/youtube/funcs_test.go
+++ sites/youtube/funcs_test.go
@@ -35,3 +35,37 @@ func TestQuickMatch(t *testing.T) {
 		}
 	}
 }
+
+func TestGetCode(t *testing.T) {
+	tests := []struct {
+		url, code string
+	}{
+		{"https://youtu.be/abcde-fg_12", "abcde-fg_12"},
+		{"http://youtu.be/abcde-fg_12", "abcde-fg_12"},
+		{"http://www.google.com/", ""},
+		{"https://www.google.com/", ""},
+		{"https://www.youtube.com/watch?v=zyx123wvu_4", "zyx123wvu_4"},
+		{"https://www.youtube.com/watch?k=v&v=zyx123wvu_4", "zyx123wvu_4"},
+		{"https://www.youtube.com/watch?k=v&v=zyx123wvu_4&y=7&t=5", "zyx123wvu_4"},
+		{"https://www.youtube.com/watch?k=v&v=zyx123wvu!4&y=7&t=5", ""},
+		{"https://www.youtube.com/watch?v=zyx123wvu4", ""},
+		{"https://www.youtube.com/watch?v=zyx123wvu456", ""},
+		{"http://www.youtube.com/watch?k=v&v=zyx123wvu_4&y=7&t=5", "zyx123wvu_4"},
+		{"http://youtube.com/watch?k=v&v=zyx123wvu_4&y=7&t=5", "zyx123wvu_4"},
+		{"https://youtube.com/watch?k=v&v=zyx123wvu_4&y=7&t=5", "zyx123wvu_4"},
+		{"https://www.youtube.com/v/youtubecode", "youtubecode"},
+		{"https://www.youtube.com/v/youtubecode?y=1", "youtubecode"},
+		{"http://www.youtube.com/v/youtubecode?y=1", "youtubecode"},
+		{"https://youtube.com/v/youtubecode?y=1", "youtubecode"},
+		{"http://youtube.com/v/youtubecode?y=1", "youtubecode"},
+		{"http://youtube.com/v/outubecode?y=1", ""},
+		{"http://youtube.com/v/yyoutubecode?y=1", ""},
+	}
+
+	for n, test := range tests {
+		code := getCode(test.url)
+		if code != test.code {
+			t.Errorf("test %d: expecting code %q, got %q", n+1, test.code, code)
+		}
+	}
+}
