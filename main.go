de4a0fac8381d32e14c5cc3d993a8172dfad99a8
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-30 17:08:03 +0000

    Rewritten to use cache (cache still needs testing)

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"time"
 
 	"github.com/MJKWoolnough/downloader"
 
+	"github.com/MJKWoolnough/downloader/cache"
 	_ "github.com/MJKWoolnough/downloader/sites/youtube"
 )
 
+var fileCache *cache.Cache
+
 func main() {
+	fileCache = cache.NewCache("/home/michael/temp/dlcache/")
 	http.ListenAndServe(":8080", http.HandlerFunc(proxy))
 }
 
@@ -32,24 +34,14 @@ func proxy(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	w.Header().Add("Content-Disposition", fmt.Sprintf("inline; filename=%q", req.Title+".mp4"))
 	d := req.Downloaders[0]
-	//c := cache.Get(d.UID, d.Sources, offset, length)
-	//io.Copy(w, c)
-	//c.Close()
-	w.Header().Add("Content-Length", fmt.Sprintf("%d", d.Size))
-	w.Header().Add("Last-Modified", d.LastModified.Format(time.RFC850))
-	w.Header().Add("Content-Type", d.MimeType)
-	//read := 0
-	//errs := 0
-	io.Copy(w, d.Sources[0])
-	/*	for read < d.Size && errs < len(d.Sources) {
-		n, err := io.Copy(w, d.Sources[errs])
-		d.Sources[errs].Close()
-		read += int(n)
-		if err != nil {
-			errs++
-			fmt.Println(err)
-		}
-	}*/
+
+	c, err := fileCache.Get(d.UID, d.Sources[0], d.Size)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Add("Content-Disposition", fmt.Sprintf("inline; filename=%q", req.Filename))
+	http.ServeContent(w, r, req.Filename, d.LastModified, c)
 }
