5a49e559cd755bad0359bceeffffe04a4c6c77fa
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-30 11:45:14 +0000

    Initial rewrite of cached.go

--- cache/cached.go
+++ cache/cached.go
@@ -1,96 +1,70 @@
 package cache
 
-import (
-	"io"
-	"os"
-)
+import "io"
 
 type CachedObject struct {
 	object *object
-	file   *os.File
 	pos    int64
 }
 
-func (c *CachedObject) Close() error {
-	if c.file != nil {
-		c.pos = 0
-		return c.file.Close()
-	}
-	return nil
-}
-
 func (c *CachedObject) Read(p []byte) (int, error) {
-	if c.file == nil {
-		return 0, FileClosed{}
+	if err := c.o.Request(c.pos, len(p)); err != nil {
+		return 0, err
 	}
-	readTo := c.pos + int64(len(p))
-	size, err := c.o.Ready(readTo)
-	if size >= readTo {
-		n, err := c.file.Read(p)
-		c.pos += int64(n)
-		return n, err
-	}
-	return 0, err
+	n, err := c.o.file.ReadAt(p, c.pos)
+	c.pos += int64(n)
+	return n, err
 }
 
 func (c *CachedObject) ReadAt(p []byte, off int64) (int, error) {
-	if c.file == nil {
-		return 0, FileClosed{}
+	if err := c.o.Request(off, len(p)); err != nil {
+		return 0, err
 	}
-	readTo := off + int64(len(p))
-	size, err := c.o.Ready(readTo)
-	if size >= readTo {
-		return c.file.ReadAt(p, off)
-	}
-	return 0, err
+	return c.o.file.ReadAt(p, off)
 }
 
-func (c *CachedObject) Seek(offset int64, whence int) (n int64, err error) {
-	if c.file == nil {
-		return 0, FileClosed{}
-	}
+func (c *CachedObject) Seek(offset int64, whence int) (int64, error) {
 	switch whence {
-	case os.SEEK_SET:
+	case 0:
 		c.pos = offset
-	case os.SEEK_CUR:
+	case 1:
 		c.pos += offset
-	case os.SEEK_END:
-		c.pos = 0
-		return 0, SeekEndError{}
+	case 2:
+		c.pos = c.o.size - offset
 	default:
-		return 0, UnknownWhence(whence)
+		c.pos = 0
+		return 0, UnknownWhence{}
 	}
 	if c.pos < 0 {
 		c.pos = 0
-		err = NegativeOffset{}
+		return 0, NegativeOffset{}
 	}
-	c.file.Seek(c.pos, os.SEEK_SET)
-	return c.pos, err
+	return c.pos, nil
 }
 
 func (c *CachedObject) WriterTo(w io.Writer) (int64, error) {
-	if c.file == nil {
-		return 0, FileClosed{}
-	}
-	var total int64
-	for {
-		size, err := c.o.Ready(c.pos + 32*1024)
-		if err == io.EOF {
-			n, err := io.Copy(w, c.file)
-			total += n
-			return total, err
-		} else if size > c.pos {
-			n, err := io.CopyN(w, c.file, size-c.pos)
-			total += n
-			c.pos += n
-			if err != nil {
-				return total, err
-			}
-		}
+	var (
+		read int64
+		err  error
+		n    int
+	)
+	buf := make([]byte, 32*1024)
+	for c.pos < c.o.size {
+		err = c.o.Request(c.pos, len(buf))
 		if err != nil {
-			return total, err
+			break
+		}
+		n, err = c.o.file.Read(buf)
+		c.pos += int64(n)
+		_, e := w.Write(buf[:n])
+		if err != nil {
+			break
+		} else if e != nil {
+			err = e
+			break
 		}
 	}
+	return read, err
 }
 
 // Errors
@@ -101,14 +75,8 @@ func (UnknownWhence) Error() string {
 	return "unknown whence"
 }
 
-type SeekEndError struct{}
-
-func (SeekEndError) Error() string {
-	return "seeking from the end is not currently supported"
-}
-
 type NegativeOffset struct{}
 
 func (NegativeOffset) Error() string {
-	return "seeked to negative offset"
+	return "can't seek to negative offset"
 }
